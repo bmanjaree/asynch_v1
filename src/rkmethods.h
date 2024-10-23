2fede58f593bb7b8e620e042dc6b8722160efd42
Manjaree Binjolkar <mb6477@princeton.edu>
2024-10-23 12:45:20 -0400

    Removed commented out function declarations

--- src/rkmethods.h
+++ src/rkmethods.h
@@ -11,18 +11,12 @@
 
 //RK methods data
 void RKDense3_2(RKMethod* method);
-//void RKDense3_2_b(double theta, double *b);
-//void RKDense3_2_bderiv(double theta, double *b);
 
 void TheRKDense4_3(RKMethod* method);
-//void TheRKDense4_3_b(double theta, double *b);
 
 void DOPRI5_dense(RKMethod* method);
-//void DOPRI5_b(double theta, double *b);
-//void DOPRI5_bderiv(double theta, double *b);
 
 void RadauIIA3_dense(RKMethod* method);
-//void RadauIIA3_b(double theta, double *b);
 
 #endif
 
