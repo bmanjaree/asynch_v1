bd3f8c08f6c1453318449cb796418b3352efe704
atmichalek <am2192@princeton.edu>
2025-06-03 07:50:53 -0400

    updated compile script for tiger

--- compile.sh
+++ compile.sh
@@ -2,6 +2,13 @@
 #!/bin/bash
 # export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/hdf5/gcc/1.14.4/include
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/hdf5/gcc/1.14.4/lib64
+
+module purge
+module load intel-mpi/gcc/2021.15
+module load hdf5/gcc/1.14.4        
+module load netcdf/gcc/hdf5-1.14.4/4.9.2
+
+
 autoreconf --install
 mkdir build
 cd build
